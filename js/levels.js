50a1477aad8574ead92efbcc85d9a5d4f110a77a
Maksym Kantor <max.kantor72@gmail.com>
2022-06-22 20:22:34 +0300

    refactor: move levels into separate file

--- js/levels.js
+++ js/levels.js
@@ -1,10 +1,4 @@
-const hintBtn = document.querySelector('.card__hint-btn');
-const hint = document.querySelector('.card__hint');
-hintBtn.addEventListener('click', () => {
-  hint.classList.toggle('_active');
-});
-
-const levels = [
+export default [
   {
     question: 'Что такое DOCTYPE и для чего он нужен?',
     answer: 'DOCTYPE - специальный тег, который указывает тип документа и его стандарт. Он должен находится в начале каждого HTML-документа.',
@@ -276,35 +270,3 @@ const levels = [
     topic: 'OOP',
   },
 ];
-let currentLevel = 0;
-
-const nextBtn = document.querySelector('.card__next-btn');
-const prevBtn = document.querySelector('.card__prev-btn');
-
-function changeLevel(source) {
-  const question = document.querySelector('.card__question');
-  const hint = document.querySelector('.card__hint');
-  const levelCounter = document.querySelector('.card__counter');
-  question.textContent = source[currentLevel].question;
-  hint.textContent = source[currentLevel].answer;
-  levelCounter.textContent = `${currentLevel + 1}/${source.length}`;
-  if(currentLevel + 1 === source.length) {
-    nextBtn.classList.add('_inactive');
-  }else if(currentLevel < 1) {
-    prevBtn.classList.add('_inactive');
-  }else {
-    nextBtn.classList.remove('_inactive');
-    prevBtn.classList.remove('_inactive');
-  }
-}
-
-nextBtn.addEventListener('click', () => {
-    currentLevel++;
-    changeLevel(levels);
-});
-prevBtn.addEventListener('click', () => {
-  currentLevel--;
-  changeLevel(levels);
-});
-
-changeLevel(levels);
